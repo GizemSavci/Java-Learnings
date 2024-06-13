88635b183e05f30f29d8ded7c175f271cbc2ee40
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-06-13 02:40:52 +0200

    Create toDos program

---
+++ toDos.java
@@ -0,0 +1,24 @@
+// This is the todo list of an one girl army developer 
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+
+    String toDo2 = "Feed the cats";
+    String toDo3 = "Save the world";
+    String toDo4 = "Land an internship at Danske Bank";
+
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
+    toDoList.add(toDo4);
+    System.out.println(toDoList);
+      
+    
+  }
+  
+}
